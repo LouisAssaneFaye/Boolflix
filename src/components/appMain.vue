1a7159fb39873e026fc28f493de93bc7929a4e27
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-06-22 20:59:10 +0200

    metà milestone 2 fatta

--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -1,6 +1,6 @@
 <template>
      <main>
-        <searchBar @search="getSearchedFilm" />
+        <searchBar @search="getSearchedFilm, getSearchedSerie" />
         <div v-for="filmObj in filmList">
             
             <div class="titolo">
@@ -24,6 +24,29 @@
 
         </div>
 
+        <div v-for="serieObj in serieList">
+            
+            <div class="titolo">
+                {{ serieObj.name }}
+
+            </div>
+            
+            <div class="titolo originale">
+                {{ serieObj.original_name }}
+
+            </div>
+             
+            <div class="lingua">
+                {{ serieObj.original_language }}
+            </div>
+            
+            <div class="voto">
+                {{ serieObj.vote_average }}
+                
+            </div>
+
+        </div>
+
      </main>
 </template>
 <script>
@@ -34,8 +57,10 @@ export default{
   data(){
     return{
         filmList:[],
+        serieList:[],
         apiUrl: 'https://api.themoviedb.org/3/search/movie?api_key=dcfd41668f40665b3e285e3308a1c9d3&query=',
         APIKey: 'dcfd41668f40665b3e285e3308a1c9d3',
+        apiUrlTwo: 'https://api.themoviedb.org/3/search/tv?api_key=dcfd41668f40665b3e285e3308a1c9d3&language=it_IT&query=',
     }
 
   },
@@ -54,10 +79,23 @@ export default{
           console.log(error);
           });
 
+        },
+        getSearchedSerie(needle=''){
+            axios.get(`${this.apiUrlTwo}${needle}`)
+           .then((response)=> {
+           console.log(response);
+           this.serieList=response.data.results;
+        
+           })
+          .catch(function (error) {
+          console.log(error);
+          });
+
         }
   },
   created(){
-        this.getSearchedFilm()
+        this.getSearchedFilm(),
+        this.getSearchedSerie()
   }
 }
 </script>
