4771b02872fb3125d45c2b9a29f586704ea53193
LouisAssaneFaye <louis.faye.18@outlook.it>
2023-06-22 21:00:30 +0200

    modifica

--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -1,6 +1,6 @@
 <template>
      <main>
-        <searchBar @search="getSearchedFilm, getSearchedSerie" />
+        <searchBar @search="getSearchedFilm" />
         <div v-for="filmObj in filmList">
             
             <div class="titolo">
